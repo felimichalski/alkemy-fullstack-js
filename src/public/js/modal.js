0a96ba424a2154797050c9f4a7ed6065ca01e838
Felipe <felimichalski@icloud.com>
2022-04-16 01:00:16 -0300

    Navigation for users

--- src/public/js/modal.js
+++ src/public/js/modal.js
@@ -1,3 +1,13 @@
+$('#user').click(() => {
+    if ($('.modal-card').css('display') === 'none') {
+        $('.modal-card').css('display', 'flex');
+        $('.modal-overlay').css('display', 'block');
+    } else {
+        $('.modal-card').css('display', 'none');
+        $('.modal-overlay').css('display', 'none');
+    }
+});
+
 $('.modal-overlay').click(() => {
     $('.modal-overlay').css('display', 'none');
     $('.modal-card').css('display', 'none');
--- src/public/js/nav.js
+++ src/public/js/nav.js
@@ -1,23 +1,17 @@
-$('#user').click(() => {
-    if ($('.modal-card').css('display') === 'none') {
-        $('.modal-card').css('display', 'flex');
-        $('.modal-overlay').css('display', 'block');
-    } else {
-        $('.modal-card').css('display', 'none');
-        $('.modal-overlay').css('display', 'none');
-    }
-});
-
+// Show/hide collapsable-navbar
 $('.expand-nav-btn').click(() => {
     $('.nav').toggleClass('expand');
-    if($('.nav-overlay').css('display') == 'none') {
-        $('.nav-overlay').css('display', 'block');
+    if($('.nav-overlay').css('visibility') == 'hidden') {
+        $('.nav-overlay').css('visibility', 'visible');
+        $('.nav-overlay').css('background-color', 'rgba(0, 0, 0, .7)');
     } else {
-        $('.nav-overlay').css('display', 'none');
+        $('.nav-overlay').css('visibility', 'hidden');
+        $('.nav-overlay').css('background-color', '');
     }
 });
 
 $('.nav-overlay').click(() => {
     $('.nav').removeClass('expand');
-    $('.nav-overlay').css('display', 'none');
+    $('.nav-overlay').css('visibility', 'hidden');
+    $('.nav-overlay').css('background-color', '');
 })
