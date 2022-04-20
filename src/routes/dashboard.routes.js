cb5129ce0643fcded9203e1a479c3073ea9fa0fb
Felipe <felimichalski@icloud.com>
2022-04-20 18:45:31 -0300

    Files separated by sections

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -5,17 +5,19 @@ const pool = require('../database');
 
 const { isLoggedIn } = require('../lib/protect');
 
-router.get('/', isLoggedIn, async(req, res) => {
 
+
+router.get('/', isLoggedIn, async(req, res) => {
+    
     const user = req.app.locals.user;
 
     const expenses = await pool.query(`SELECT E_VALUE, CREATED_AT, CATEGORY FROM expenses WHERE ID_CLIENT = ${user.ID_CLIENT}`);
     const income = await pool.query(`SELECT I_VALUE, CREATED_AT, CATEGORY FROM income WHERE ID_CLIENT = ${user.ID_CLIENT}`);
     const result = expenses.concat(income)
+
+    const rows = result.sort((a, b) => b.CREATED_AT - a.CREATED_AT).slice(0, 10); // Sorting last 10 income and expenses by date
     
-    const rows = result.sort((a, b) => b.CREATED_AT - a.CREATED_AT).slice(0, 10); // Sorting income and expenses by date to show in a chart
-    
-    res.render('dashboard/main', {operations: rows});
+    res.render('user/dashboard', {operations: rows});
 });
 
 
