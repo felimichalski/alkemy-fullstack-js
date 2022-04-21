3d4a6960e3d44f68f85bd1d168106f25e1afddcd
Felipe <felimichalski@icloud.com>
2022-04-21 00:30:48 -0300

    Pagination method changed

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -25,8 +25,8 @@ router.get('/list', isLoggedIn, async(req, res) => {
 
     const user = req.app.locals.user;
 
-    const expenses = await pool.query(`SELECT ID_OPERATION, E_VALUE, CREATED_AT, CATEGORY FROM expenses WHERE ID_CLIENT = ${user.ID_CLIENT}`);
-    const income = await pool.query(`SELECT ID_OPERATION, I_VALUE, CREATED_AT, CATEGORY FROM income WHERE ID_CLIENT = ${user.ID_CLIENT}`);
+    const expenses = await pool.query(`SELECT E_ID_OPERATION, E_VALUE, CREATED_AT, CATEGORY FROM expenses WHERE ID_CLIENT = ${user.ID_CLIENT}`);
+    const income = await pool.query(`SELECT I_ID_OPERATION, I_VALUE, CREATED_AT, CATEGORY FROM income WHERE ID_CLIENT = ${user.ID_CLIENT}`);
     const result = expenses.concat(income)
 
     const pages = Math.ceil(result.length / 10); // Counting number of pages every 10 items
@@ -36,37 +36,40 @@ router.get('/list', isLoggedIn, async(req, res) => {
         return res.redirect('/dashboard/list'); // Preventing the user from manually entering a non-existent page
     }
     
-    const rows = result.sort((a, b) => b.CREATED_AT - a.CREATED_AT).slice((page - 1) * 10, page * 10); // Sorting 10 income and expenses by date according the page
-    
     let totalPages = [];
     for(let i = 1; i <= pages; i++) {
         totalPages.push(i); // Creating an array of pages
     }
-
-    if(totalPages.length > 3) { // Reducing number of pages to 3
-        if(page == 1) {
-            totalPages = totalPages.slice(0, 3); // Show active and next two if is the first page
-        } else if(page == totalPages.length) {
-            totalPages = totalPages.slice(totalPages.length - 3, totalPages.length); // Show active and previous two if is the last page
-        } else {
-            totalPages = totalPages.slice(page - 2, page + 1); // Show active, previuos and next
-        }
-
-        if(page - 2 > 0) {
-            totalPages.unshift('prev'); // Active has 2 previuos pages
-        }
-
-        if(page + 2 <= pages) {
-            totalPages.push('next') // Active has 2 next pages
-        }
-    }
+    
+    const rows = result.sort((a, b) => b.CREATED_AT - a.CREATED_AT).slice((page - 1) * 10, page * 10); // Sorting 10 income and expenses by date according the page
 
     if(rows.length > 1) { // Checking that the page has more than 1 row
         res.render('user/list', {operations: rows, first: (page - 1) * 10 + 1, last: (page == pages)?result.length:page * 10, totalPages, active: page});
     } else {
         res.render('user/list', {operations: rows, first: undefined, last: (page == pages)?result.length:page * 10, totalPages, active: page});
     }
+});
 
+router.post('/list/delete/:id_operation', async (req, res) => {
+    let operation = req.params.id_operation;
+    if(operation.charAt(0) == 'e') {
+        try {
+            await pool.query('DELETE FROM expenses WHERE E_ID_OPERATION = ?', [operation.slice(1)]);
+            req.flash('success', 'Operation deleted successfully');
+        } catch (err) {
+            console.log(err)
+            req.flash('message', 'An error has ocurred, please try again later')
+        }
+    } else {
+        try {
+            await pool.query('DELETE FROM income WHERE I_ID_OPERATION = ?', [operation.slice(1)]);
+            req.flash('success', 'Operation deleted successfully');
+        } catch (err) {
+            console.log(err)
+            req.flash('message', 'An error has ocurred, please try again later')
+        }
+    }
+    res.redirect('/dashboard/list');
 });
 
 
